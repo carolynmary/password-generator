88071c855c40079228b148edf1ae6fe6088e9414
CaroS <carolyn.mary@hotmail.com>
2020-08-29 02:30:12 -0400

    updated and final js code

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # password-generator
 
+LIVE SITE:
+https://carolynstolze.github.io/password-generator.github.io/
+
+GITHUB REPOSITORY:
+https://github.com/carolynstolze/password-generator.github.io/blob/master/script.js
+
+- - -
+
 ## commit 1
 - initial push: files copied from homework assignment. No code added.
 
@@ -14,3 +22,6 @@
 
 ## commit 5
 - updated js code after TA and Tutor input
+
+## commit 6
+- Working password generator with updated js code!
--- script.js
+++ script.js
@@ -1,117 +1,67 @@
-// Assignment Code
+// characters to be included in password
+var specialCharacters = ["@", "%", "+", "/", "'", "!", "#", "$", "^", "?", ":", ",", ")", "(", "}", "{", "]", "[", "~", "-", "_", ".",];
+var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var lowerCasedCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+var upperCasedCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
+
+// Var to store user's password after it's generated
+var usersPassword = ""
+
+// ======= (START) Function to generate password with user's input =======
+function generatePasswordStart() {
+  // Var to store user's input
+  var usersCharCriteria = [];
+  // Collecting user's password criteria:
+  var passwordLength = prompt("How many characters would you like your password to be? (min 8, max 128)");
+  if (passwordLength < 8 || passwordLength > 128) { // checking if character length is ok
+    alert("Password must be between 8 and 128 characters. Please start again.");
+    return;
+  }
+  if (passwordLength !== Number) { // checking if answer is a number
+    alert("Password length must be entered as a number. Please start again.");
+    return;
+  }
+  var wantLowercase = confirm("Would you like your password to contain lowercase characters?")
+  var wantUppercase = confirm("Would you like your password to contain uppercase characters?")
+  var wantSpecial = confirm("Would you like your password to contain special characters?")
+  var wantNumbers = confirm("Would you like your password to contain numbers?")
+  if (wantLowercase === false && wantUppercase === false && wantSpecial === false && wantNumbers === false) { // checking that at least one confirm is true
+    alert("At least one character type must be selected. Please start again.");
+    return;
+  }
+  // NESTED FUNCTION: Generating password according to user's password criteria
+  function generatePassword() {
+    // For loop to push one random (true)character to [usersCharCriteria]
+    for (var i = 0; i < passwordLength; i++) {
+      if (wantLowercase) {
+        usersCharCriteria.push(lowerCasedCharacters[Math.floor(Math.random() * 10)])
+      }
+      if (wantUppercase) {
+        usersCharCriteria.push(upperCasedCharacters[Math.floor(Math.random() * 10)])
+      }
+      if (wantSpecial) {
+        usersCharCriteria.push(specialCharacters[Math.floor(Math.random() * 10)])
+      }
+      if (wantNumbers) {
+        usersCharCriteria.push(numericCharacters[Math.floor(Math.random() * 10)])
+      }
+    }
+    // Converts generated password from an array to a string and slices it to the selected password length
+    usersPassword = usersCharCriteria.join("").slice(0, passwordLength);
+    return usersPassword;
+  }
+  // Function call(s)
+  generatePassword();
+} // ======= (END) generatePasswordStart() =======
+
+// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Function to display generated password to the #password text area
 function start() {
-  console.log("we're here!");
-  var password = generatePasswordStart();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
+  generatePasswordStart();
+  document.querySelector("#password").innerHTML = usersPassword;
 }
 
-//return a string
-function generatePasswordStart() {
-
-  // ======= vars definitions ======
-
-  // Array of special characters to be included in password
-  var specialCharacters = ["@", "%", "+", "/", "'", "!", "#", "$", "^", "?", ":", ",", ")", "(", "}", "{", "]", "[", "~", "-", "_", ".",];
-  // Array of numeric characters to be included in password
-  var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  // Array of lowercase characters to be included in password
-  var lowerCasedCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
-  // Array of uppercase characters to be included in password
-  var upperCasedCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
-  var userOptionalChars = [];
-
-  // ======= functions definitions ======
-
-  // FUNCTION 1: gets user options and store in vars
-  // function getUserOptions() {
-
-    // asking CHARACTER LENGTH: it should be at least 8 chars and no more than 128 chars
-    var passwordLength = prompt("How many characters would you like your password to be? (min 8, max 128)");
-    // ----checking if character length is ok----
-    if (passwordLength < 8 || passwordLength > 128) {
-      alert("Password must be between 8 and 128 characters.");
-      return;
-    }
-    // asking LOWERCASE INCLUDED?
-    var wantLowercase = confirm("Would you like your password to contain lowercase characters?")
-    // asking UPPERCASE INCLUDED?
-    var wantUppercase = confirm("Would you like your password to contain uppercase characters?")
-    // asking SPECIAL CHARACTERS INCLUDED?
-    var wantSpecial = confirm("Would you like your password to contain special characters?")
-    // asking NUMBERS INCLUDED?
-    var wantNumbers = confirm("Would you like your password to contain numbers?")
-    // ----checking that at least one is true----
-    if (wantLowercase === false && wantUppercase === false && wantSpecial === false && wantNumbers === false) {
-      alert("At least one character type must be selected.");
-      return;
-    }
-
-    // NESTED FUNCTION A: generates password according to user selections
-    // function generatePassword(userOptions) {
-      // var userOption = getUserOptions();
-      // Variable to store password as it's being concatenated
-      var password = [];
-
-      // if lowercase was selected by user:
-      if (wantLowercase) {
-        // push one random lower char to password
-        userOptionalChars.push(lowerCasedCharacters)
-        // add lowerCharsArray to userOptionalChars
-      }
-
-      // if uppercase was selected by user:
-      if (wantUppercase) {
-        // push a random upper char to password
-        userOptionalChars.push(upperCasedCharacters)
-        // add upperCharsArray to userOptionalChars
-      }
-
-      // if special characters was selected by user:
-      if (wantSpecial) {
-        // push a random special char to password
-        userOptionalChars.push(specialCharacters)
-        // add specialCharsArray to userOptionalChars
-      }
-
-      // if numeric was selected by user:
-      if (wantNumbers) {
-        // push a random numeric char to password
-        userOptionalChars.push(numericCharacters)
-        // add numericCharsArray to userOptionalChars
-      }
-
-      // for loop between start number of elements in password to the requested number of characters
-      for (var i = 0; i < passwordLength.length; i++) {
-        password.push(userOptionalChars[Math.floor(Math.random()*passwordLength.length)]);
-        
-      }
-
-      // mutate the array to a string
-      // return password string
-      // return result.join('');
-      console.log(password)
-    // }
-
-    // NESTED FUNCTION B: calls (start) 
-    // getUserOptions();
-    // generatePassword(userOption);
-  // }
-}
-
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", start);
-  // var expectSpecialChars = false;
-  // var expectNumericChars = false;
-  // var expectUppercaseChars = false;
-  // var expectLowercaseChars = false;
-  // var userOptions = {
-  //   expectSpecialChars: false,
-  //   expectNumericChars: false,
-  //   expectUppercaseChars: false,
-  //   expectLowercaseChars: false,
-
-  // generatePassword()  // this line added by harrison
+// Adds event listener to generate button
+generateBtn.addEventListener("click", start);
